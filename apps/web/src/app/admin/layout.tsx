c8699f54348370004f8811f70032daf208a5c6fe
Alberto Balderas <abalderas10@gmail.com>
2026-04-11 11:36:33 -0600

    fix(admin): desactivar cache para evitar mismatch de chunks

--- apps/web/src/app/admin/layout.tsx
+++ apps/web/src/app/admin/layout.tsx
@@ -1,5 +1,8 @@
 import { AdminLayout } from "@/components/admin-layout";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function Layout({
   children,
 }: Readonly<{
